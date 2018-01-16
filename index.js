f8a33871c4533dabc139b723a0133c0ac7ec167b
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 17:22:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+let driver_id = 0;
+
+class Driver {
+  constructor (name) {
+    this.id = ++driver_id;
+    this.name = name;
+  }
+
+  trips() {
+    
+  }
+}
+
+let passenger_id = 0;
+
+class Passenger {
+  constructor (name) {
+    this.id = ++gassenger_id;
+    this.name = name
+  }
+  
+}
+
+let trip_id = 0;
+
+class Trip {
+  constructor (driverId, passengerId) {
+    this.id = ++trip_id;
+    this.driverId = driverId;
+    this.passengerId = passengerId;
+  }
+  
+}
